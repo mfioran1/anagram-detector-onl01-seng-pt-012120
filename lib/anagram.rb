0b889c931a6d1c59c842fdf1727385adf9b1de3a
Maurice Fiorani <mauricef97@gmail.com>
2020-02-14 21:09:24 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,15 @@
-class Anagram
-  attr_accessor :word
-
+ class Anagram
+   
+  attr_accessor :word 
+  
   def initialize(word)
     @word = word
   end
   
-  def match(word_array)
-    word_array.select do |word|
-      word.split("").sort == @word.split("").sort
-    end
+  def match(array_of_words)
+    array_of_words.split("").sort == @word.split("").sort
   end
+  
 end  
+  
+   
